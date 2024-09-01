2fb09e63f48ed7907f4ca57dc1739495c1f1727f
Floris Stoica <floris@nusunt.eu>
2024-09-02 01:55:13 +0300

    tweak whichkey

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -2,6 +2,32 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   lazy = false,
+  provider = "ollama",
+  vendors = {
+       ollama = {
+         ["local"] = true,
+         endpoint = "127.0.0.1:11434/v1",
+         model = "codegemma",
+         parse_curl_args = function(opts, code_opts)
+           return {
+             url = opts.endpoint .. "/chat/completions",
+             headers = {
+               ["Accept"] = "application/json",
+               ["Content-Type"] = "application/json",
+             },
+             body = {
+               model = opts.model,
+               messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
+               max_tokens = 2048,
+               stream = true,
+             },
+           }
+         end,
+         parse_response_data = function(data_stream, event_state, opts)
+           require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+         end,
+       },
+  },
   opts = {
     -- add any opts here
   },
@@ -12,6 +38,7 @@ return {
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
   },
 }
 
