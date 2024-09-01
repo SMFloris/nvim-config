bfcba4f080866d32ca438206a5c0965ec1e70476
Floris Stoica <floris@nusunt.eu>
2024-09-02 02:35:00 +0300

    tweak whichkey

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -19,7 +19,7 @@ return {
             body = {
               model = opts.model,
               messages = require("avante.providers").claude.parse_message(code_opts),  -- you can make your own message, but this is very advanced
-              max_tokens = 2048,
+              max_tokens = 4096,
               stream = true,
             },
           }
