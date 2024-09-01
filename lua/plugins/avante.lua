ea9463c1a1e8ac4888fd758a84d01904f2a32c50
Floris Stoica <floris@nusunt.eu>
2024-09-02 01:59:32 +0300

    tweak whichkey

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -2,33 +2,33 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   lazy = false,
-  provider = "ollama",
   opts = {
-  vendors = {
-       ollama = {
-         ["local"] = true,
-         endpoint = "127.0.0.1:11434/v1",
-         model = "codegemma",
-         parse_curl_args = function(opts, code_opts)
-           return {
-             url = opts.endpoint .. "/chat/completions",
-             headers = {
-               ["Accept"] = "application/json",
-               ["Content-Type"] = "application/json",
-             },
-             body = {
-               model = opts.model,
-               messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
-               max_tokens = 2048,
-               stream = true,
-             },
-           }
-         end,
-         parse_response_data = function(data_stream, event_state, opts)
-           require("avante.providers").openai.parse_response(data_stream, event_state, opts)
-         end,
-       },
-  },
+    provider = "ollama",
+    vendors = {
+      ollama = {
+        ["local"] = true,
+        endpoint = "127.0.0.1:11434/v1",
+        model = "codegemma",
+        parse_curl_args = function(opts, code_opts)
+          return {
+            url = opts.endpoint .. "/chat/completions",
+            headers = {
+              ["Accept"] = "application/json",
+              ["Content-Type"] = "application/json",
+            },
+            body = {
+              model = opts.model,
+              messages = require("avante.providers").copilot.parse_message(code_opts),  -- you can make your own message, but this is very advanced
+              max_tokens = 2048,
+              stream = true,
+            },
+          }
+        end,
+        parse_response_data = function(data_stream, event_state, opts)
+          require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+        end,
+      },
+    },
   },
   build = ":AvanteBuild", -- This is optional, recommended tho. Also note that this will block the startup for a bit since we are compiling bindings in Rust.
   dependencies = {
@@ -37,7 +37,6 @@ return {
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    "zbirenbaum/copilot.lua",      -- for providers='copilot'
   },
 }
-
