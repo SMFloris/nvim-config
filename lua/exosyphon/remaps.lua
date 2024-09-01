f12e35090193f10fa2ca6c3bbffe114fa54b0fc7
Floris Stoica <floris@nusunt.eu>
2024-09-01 23:11:09 +0300

    tweak whichkey

--- lua/exosyphon/remaps.lua
+++ lua/exosyphon/remaps.lua
@@ -110,6 +110,11 @@ vim.api.nvim_create_autocmd("TermOpen", {
   end,
 })
 
+local function toggleNvimTreeWithoutFocus()
+  local tree = require('nvim-tree.api').tree;
+  tree.open({focus = false});
+end
+
 -- highlight yanks
 vim.api.nvim_create_autocmd("TextYankPost", {
   group    = "custom_buffer",
@@ -153,46 +158,10 @@ vim.api.nvim_create_autocmd({'BufEnter', 'QuitPre'}, {
     end
   end
 })
-vim.api.nvim_create_autocmd({'BufEnter', 'QuitPre'}, {
-  nested = false,
-  callback = function(e)
-    local tree = require('nvim-tree.api').tree
-
-    -- Nothing to do if tree is not opened
-    if not tree.is_visible() then
-      return
-    end
-
-    -- How many focusable windows do we have? (excluding e.g. incline status window)
-    local winCount = 0
-    for _,winId in ipairs(vim.api.nvim_list_wins()) do
-      if vim.api.nvim_win_get_config(winId).focusable then
-        winCount = winCount + 1
-      end
-    end
-
-    -- We want to quit and only one window besides tree is left
-    if e.event == 'QuitPre' and winCount == 2 then
-      vim.api.nvim_cmd({cmd = 'qall'}, {})
-    end
-
-    -- :bd was probably issued an only tree window is left
-    -- Behave as if tree was closed (see `:h :bd`)
-    if e.event == 'BufEnter' and winCount == 1 then
-      -- Required to avoid "Vim:E444: Cannot close last window"
-      vim.defer_fn(function()
-        -- close nvim-tree: will go to the last buffer used before closing
-        tree.toggle({find_file = true, focus = true})
-        -- re-open nivm-tree
-        tree.toggle({find_file = true, focus = false})
-      end, 10)
-    end
-  end
-})
 
 -- Neotree
 wk.add({"<leader>e", group = "File explorer"})
-vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", {desc = "Show NvimTree"})
+vim.keymap.set("n", "<leader>e", toggleNvimTreeWithoutFocus, {desc = "Show NvimTree"})
 wk.add({"<leader>ee", proxy="<leader>E", group = "File explorer", desc = "Focus NvimTree"})
 wk.add({"<leader>E", hidden = true})
 vim.keymap.set("n", "<leader>E", "<cmd>NvimTreeFocus<CR>", {desc = "Focus NvimTree"})
