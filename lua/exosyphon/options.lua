bd0c18b695bc476212cd98b31e88f0455bca2fc0
Floris Stoica <floris@nusunt.eu>
2024-11-05 23:43:48 +0200

    removed marks plugin

--- lua/exosyphon/options.lua
+++ lua/exosyphon/options.lua
@@ -30,6 +30,6 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 vim.opt.conceallevel = 0
-vim.opt.clipboard:append("unnamedplus")
+vim.opt.clipboard = "unnamedplus"
 
 vim.opt.termguicolors = true
