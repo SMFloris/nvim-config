a23d65e05b11835086cd77ff99de1798a5211657
Floris Stoica <floris@nusunt.eu>
2024-12-03 03:57:56 +0200

    small changes

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -376,7 +376,7 @@ return {
       })
     end,
   },
-  { "nvim-telescope/telescope-fzf-native.nvim",    build = "make" },
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
   {
     "folke/tokyonight.nvim",
   },
