a68936978f3c7003e08be0e1123d50d3d4e663b3
Floris Stoica <floris@nusunt.eu>
2024-12-26 14:20:57 +0200

    changed codecompanion ollama default model

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -8,13 +8,16 @@ return {
     require("codecompanion").setup({
       strategies = {
         cmd = {
-          adapter = "ollama"
+          adapter = "ollama",
+          model = "qwen2.5-coder:32b",
         },
         chat = {
           adapter = "ollama",
+          model = "qwen2.5-coder:32b",
         },
         inline = {
           adapter = "ollama",
+          model = "qwen2.5-coder:32b",
         },
       },
     })
