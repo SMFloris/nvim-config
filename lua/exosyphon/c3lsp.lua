5f3c196d0f0c25b5c4b5d5ce6984d2585aaa5608
Floris Stoica <floris@nusunt.eu>
2025-01-15 19:22:56 +0200

    configure c3lsp

--- lua/exosyphon/c3lsp.lua
+++ lua/exosyphon/c3lsp.lua
@@ -4,7 +4,7 @@ local configs = require('lspconfig.configs')
 if not configs.c3_lsp then
   configs.c3_lsp = {
     default_config = {
-      cmd = { "c3lsp" },
+      cmd = { "c3-lsp" },
       filetypes = { "c3", "c3i" },
       root_dir = function(fname)
           return util.find_git_ancestor(fname) or vim.fn.getcwd()
